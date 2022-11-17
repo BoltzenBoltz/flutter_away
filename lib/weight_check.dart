ea01307d4892155547cc02855816b32886e30275
BoltzenBoltz <68844207+BoltzenBoltz@users.noreply.github.com>
2022-11-17 19:22:58 +0100

    first version of hompage created
    design and co. is missing...

--- lib/weight_check.dart
+++ lib/weight_check.dart
@@ -1,39 +1,18 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_away/data_storage.dart';
+import 'package:flutter_away/homepage.dart';
 import 'package:flutter_away/input.dart';
 import 'package:flutter_away/page_preset.dart';
-import 'package:flutter_away/homepage.dart';
 import 'package:flutter_away/weight_collector.dart';
 
-void main() {
-  runApp(const Homepage());
-}
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Away',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class WeightCheck extends StatefulWidget {
+  const WeightCheck({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<WeightCheck> createState() => _WeightCheckState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _WeightCheckState extends State<WeightCheck> {
   DataStorage storage = DataStorage();
   WeightCollector collector = WeightCollector();
   bool startup = false;
@@ -105,13 +84,12 @@ class _MyHomePageState extends State<MyHomePage> {
               decoration: BoxDecoration(color: Colors.blue),
               child: Text('Optionen')),
           ListTile(
-            title: const Text('Einstellungen'),
+            title: const Text('Home'),
             onTap: () {
               // close drawer
               Navigator.pop(context);
               // navigate to Settings-Page
-              Navigator.push(context,
-                  MaterialPageRoute(builder: ((context) => const InputPage())));
+              Navigator.pop(context);
               // Navigator.pop(context);
             },
           ),
